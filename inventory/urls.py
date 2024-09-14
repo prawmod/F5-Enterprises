fb6e48c56bb0ea89b717ba70cc38cfac06ea3d35
Pramod Ghimire <ghimireprawmod@gmail.com>
2024-09-14 08:53:39 +0545

    Login and Register

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,7 +1,13 @@
+from tkinter.font import names
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.homepage, name='homepage'),
+    path('register/', views.register, name='register'),
+    path('login/', views.register, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+
 ]
 
--- inventory/views.py
+++ inventory/views.py
@@ -1,7 +1,10 @@
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.db.models.fields import return_None
+#from django.db.models.fields import return_None
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.template.context_processors import request
+
 
 # Create your views here.
 
@@ -20,8 +23,7 @@ def register(request):
             return redirect('homepage')
     else:
         form=UserCreationForm()
-
-return render(request, 'inventory/register.html',{'form': form})
+    return render(request, 'inventory/register.html',{'form': form})
 
 #login View
 def login_view(request):
@@ -30,3 +32,14 @@ def login_view(request):
         if form.is_valid():
             username= form.cleaned_data.get('username')
             password= form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('homepage')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'inventory/login.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('homepage')
